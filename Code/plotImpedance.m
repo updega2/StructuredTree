7c86d10bbdb2dbc24c6aeba5b4b0e333d927e598
adam <updega2@berkeley.edu>
2014-05-13 18:05:26 -0700

    added impedance plot and the report folder

---
+++ Code/plotImpedance.m
@@ -0,0 +1,9 @@
+function plotImpedance(omegas,zTree)
+x0 = floor(length(omegas)/2)+1;
+
+plot(omegas(x0:end),zTree(x0:end));
+%axis([0 500 0 10^6]);
+xlabel('Omega (2*pi*f)');
+ylabel('Impedance (Z)');
+
+end
